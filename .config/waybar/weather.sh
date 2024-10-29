719482efef986c6dfab027f36b6d2fde230aaca8
as <scorpp13@gmail.com>
2024-10-29 21:36:47 +0200

    Update

---
+++ .config/waybar/weather.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+text=$(curl ru.wttr.in/$1?format=1)
+
+if [[ $? == 0 ]]
+then
+    text=$(echo "$text" | sed -E "s/\s+/ /g")
+    tooltip=$(curl ru.wttr.in/$1?format=%l:+%C+%c+%t+%w+%m)
+    if [[ $? == 0 ]]
+    then
+        tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+        echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
+        exit
+    fi
+fi
+
+echo "{\"text\":\"Service Unavailable\", \"tooltip\":\"Service Unavailable\"}
