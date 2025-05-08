6b04c6c6e3340131ecad9f0b3d299e60a8bd7e10
as <scorpp13@gmail.com>
2025-05-08 13:31:28 +0200

    upd

--- .local/bin/script.sh
+++ .local/bin/script.sh
@@ -2,29 +2,26 @@
 
 option1="󱗄	Calculator"
 option2="	Font search"
-option3="	Convert to gif"
-option4="	Convert to jpg"
-option5="󰙴	Merge images"
-option6="	 Archive extractor"
-option7="	Package manager"
+option3="	Convert to jpg"
+option4="󰙴	Merge images"
+option5="	 Archive extractor"
+option6="	Package manager"
 
-options="$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7"
+options="$option1\n$option2\n$option3\n$option4\n$option5\n$option6"
 
 choice=$(echo -e "$options" | fuzzel -d) 
 
 case $choice in
 	"$option1")
-		alacritty -e qalc ;;
+		kitty --class floating -e qalc ;;
 	"$option2")
-		kitty --class floating "$SHELL" -c "fontsearch.sh" ;;
+		kitty --class floating -e fontsearch.sh ;;
 	"$option3")
-		alacritty -e $SHELL -c "convert-to-gif.sh && sleep 1" ;;
+		kitty --class floating -e convert-to-jpg.sh ;;
 	"$option4")
-		alacritty -e $SHELL -c "convert-to-jpg.sh && sleep 1" ;;
+		kitty --class floating -e merge_img.sh ;;
 	"$option5")
-		alacritty -e $SHELL -c "merge_img.sh && sleep 1" ;;
+		kitty --class floating -e extract.sh ;;
 	"$option6")
-		alacritty -e $SHELL -c "extract.sh && sleep 1" ;;
-	"$option7")
-		alacritty -e xbpspkg.sh ;;
+		kitty --class floating -e xbpspkg.sh ;;
 esac
