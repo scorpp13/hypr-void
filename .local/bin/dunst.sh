52d3b55d544446c7258c057588ddcb937eea95f3
scorpp13 <scorpp13@gmail.com>
2026-03-04 22:56:20 +0300

    newcfg

--- .local/bin/dunst.sh
+++ .local/bin/dunst.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/bash
 
 config=$HOME/.config/dunst/dunstrc
-head -n -2 "$config" > dunstrc_
 
 if grep -q "top" ~/.cache/.themestyle.sh ; then
-		 origin="top-center"
+		 origin="top-right"
 	elif grep -q "bottom" ~/.cache/.themestyle.sh ; then
-		 origin="bottom-center"
-	else origin="top-center"
+		 origin="bottom-right"
+	else origin="top-right"
 fi
 
+head -n -2 "$config" > dunstrc_
 echo -e "[global]\norigin = $origin" >> dunstrc_
 mv dunstrc_ "$config"
-
-pkill dunst
-sleep 0.2
-dunst
+dunstctl reload
