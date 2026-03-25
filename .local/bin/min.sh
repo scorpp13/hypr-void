8437a423080ef1da849c99419a65b1f6dd6aae0d
scorpp13 <scorpp13@gmail.com>
2026-03-25 22:06:07 +0300

    new_conf hypr 0.54

---
+++ .local/bin/min.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+if [[ -z $(hyprctl workspaces | grep special:magic) ]]; then
+    hyprctl dispatch movetoworkspacesilent special:magic
+else
+    hyprctl --batch 'dispatch togglespecialworkspace magic;dispatch movetoworkspace +0'
+fi
