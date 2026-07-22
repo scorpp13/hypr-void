b0300c2d53449d5ba27c076abd508520d37ea832
scorpp13 <scorpp13@gmail.com>
2026-07-22 23:23:01 +0300

    update dunst

--- .local/bin/dunst.sh
+++ .local/bin/dunst.sh
@@ -2,6 +2,14 @@
 
 config=$HOME/.config/dunst/dunstrc
 
+icon=$(cat ${HOME}/.config/gtk-3.0/settings.ini | \
+grep "gtk-icon-theme-name" | \
+sed 's/gtk-icon-theme-name=//')
+
+font=$(cat ${HOME}/.config/gtk-3.0/settings.ini | \
+grep "gtk-font-name" | \
+sed 's/gtk-font-name=//')
+
 if grep -q "top" ~/.cache/.themestyle.sh ; then
 		 origin="top-right"
 	elif grep -q "bottom" ~/.cache/.themestyle.sh ; then
@@ -9,7 +17,7 @@ if grep -q "top" ~/.cache/.themestyle.sh ; then
 	else origin="top-right"
 fi
 
-head -n -2 "$config" > dunstrc_
-echo -e "[global]\norigin = $origin" >> dunstrc_
+head -n -4 "$config" > dunstrc_
+echo -e "[global]\norigin = $origin\nfont = $font\nicon_theme = $icon" >> dunstrc_
 mv dunstrc_ "$config"
 dunstctl reload
