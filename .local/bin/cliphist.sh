7f1e3212f2278fbaf1f4d9d4fcc014fc70010042
scorpp13 <scorpp13@gmail.com>
2026-07-26 23:33:26 +0300

    update scripts

--- .local/bin/cliphist.sh
+++ .local/bin/cliphist.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 case $1 in
-	d) cliphist list | fuzzel -d| cliphist delete
-		;;
+	d)	cliphist list | fuzzel -d --with-nth 2 | cliphist delete
+	;;
 
-	w) if [ $(echo -e "Clear\nCancel" | fuzzel -d) == "Clear" ] ; then
+	w)	if [ $(echo -e "Wipe clipboard\nCancel" | fuzzel -d) == "Wipe clipboard" ] ; then
 		cliphist wipe
-	fi
-		;;
+		fi
+	;;
 
-   *) cliphist list | fuzzel -d| cliphist decode | wl-copy
-	   ;;
+   *)	cliphist list | fuzzel -d --with-nth 2 | cliphist decode | wl-copy
+   ;;
 esac
