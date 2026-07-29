5a5bf0a81db3b9fc0901c32f5f9d52a3f1f2b34e
scorpp13 <scorpp13@gmail.com>
2026-07-29 22:49:03 +0300

    update scripts

--- .local/bin/themeswitcher.sh
+++ .local/bin/themeswitcher.sh
@@ -10,18 +10,17 @@ listNames=""
 # Read themes list
 sleep 0.2
 options=$(find "$themes_path" -type d)
-for value in $options
-do
+for value in $options; do
 	if [ ! "$value" == "$themes_path" ]; then
 		if [ "$(find "$value" -type d | wc -l)" = 1 ]; then
 			result=$(echo "$value" | sed "s#$HOME/.config/waybar/themes/#/#g")
 			IFS='/' read -ra arrThemes <<< "$result"
-			listThemes[${#listThemes[@]}]="$arrThemes$result"
+			listThemes[${#listThemes[@]}]="/${arrThemes[1]};$result"
 			if [ -f "$themes_path$result"/config.sh ]; then
 				source "$themes_path$result"/config.sh
 				listNames+="$theme_name\n"
-				else
-				listNames+="$arrThemes$result\n"
+			else
+				listNames+="/${arrThemes[1]};$result\n"
 			fi
 		fi
 	fi
